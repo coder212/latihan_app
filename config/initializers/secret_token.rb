b2e66f56dea4fcbfc14d04d6a017743a124b81eb
coder212 <yujimaarif.ym@gmail.com>
2012-02-15 10:44:03 +0700

    learn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LatihanApp::Application.config.secret_token = 'cd4cb796e57af67d3c62cfd8566897b517ee9e849384929c5342ed1c7b394609e2d189725f75208b1454562d02b01cec4a072ab06fc9a7d4101fd00ed015ad78'
